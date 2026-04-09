1c4ecf572b1f0b22d947e566cb9ec126c637d4ca
Akmall10 <akmalprasety53@gmail.com>
2026-04-09 18:17:05 +0700

    Menambahkan Clearfrom

--- Form1.cs
+++ Form1.cs
@@ -228,35 +228,12 @@ namespace CRUDMahasiswaADO
 
        
 
-        
-
         // ───────────────────────────────────────────
-        // CLEAR FORM
+        // CELL CLICK – ISI FORM DARI GRID
         // ───────────────────────────────────────────
-        private void ClearForm()
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            txtNIM.Clear();
-            txtNama.Clear();
-            cmbJK.SelectedIndex = -1;
-            txtAlamat.Clear();
-            txtKodeProdi.Clear();
-            dtpTanggalLahir.Value = DateTime.Now;
-            txtNIM.Focus();
+            
         }
 
-        private void dataGridView1_CellClick_1(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex >= 0)
-            {
-                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
-
-                txtNIM.Text = row.Cells["NIM"].Value.ToString();
-                txtNama.Text = row.Cells["Nama"].Value.ToString();
-                cmbJK.Text = row.Cells["JenisKelamin"].Value.ToString();
-                dtpTanggalLahir.Value = Convert.ToDateTime(row.Cells["TanggalLahir"].Value);
-                txtAlamat.Text = row.Cells["Alamat"].Value.ToString();
-                txtKodeProdi.Text = row.Cells["KodeProdi"].Value.ToString();
-            }
-        }
-    }
-}
+       
